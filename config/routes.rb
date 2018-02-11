e2026b9857eaf7ca9d0bade25fc437dc4abaa4fc
Alejandro <alejandroarcos2015@gmail.com>
2018-02-11 15:28:06 -0500

    Show page without style

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   devise_for :installs
-  resources :pictures
+  resources :pictures do
+    resources :comments
+  end
   root 'pictures#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
